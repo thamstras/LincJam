7e5da8431efd1d1ea85334ed80f3d11b12e4811a
Thamstras <thomas_casson@btinternet.com>
2015-12-05 11:05:48 +0000

    We have a timer!

--- JincJam/Assets/TimerControllerScript.cs
+++ JincJam/Assets/TimerControllerScript.cs
@@ -1,15 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class TimerControllerScript : MonoBehaviour {
 
+	//How many seconds does this timer last?
+	public int countTime = 300;
+
+	private Text textBox;
+	private float timeRemaining;
+
+	string MakeTextString(int t)
+	{
+
+		int a = t / 60;
+		int b = t % 60;
+		string partA = a.ToString ();
+		string partB = b.ToString ();
+		if (partA.Length < 2)
+			partA = string.Concat("0", partA);
+		if (partB.Length < 2)
+			partB = "0" + partB;
+		return string.Concat (partA, ":", partB);
+	}
+
 	// Use this for initialization
 	void Start () {
-	
+		textBox = GetComponent<Text> ();
+		timeRemaining = countTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		timeRemaining -= Time.deltaTime;
+
+		textBox.text = MakeTextString (Mathf.CeilToInt (timeRemaining));
 	}
 }
