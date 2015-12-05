39c4643afbd14bd41c4d7bada444b227839802e2
Thamstras <thomas_casson@btinternet.com>
2015-12-05 13:48:48 +0000

    CAMERA SHACK!

---
+++ JincJam/Assets/camShake.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camShake : MonoBehaviour {
+
+	public float shakeScale = 1.0f;
+	public float xScale = 1.0f;
+	public float yScale = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xOff = shakeScale * Mathf.PerlinNoise (Time.time * xScale, 0.0f);
+		float yOff = shakeScale * Mathf.PerlinNoise (0.0f, Time.time * yScale);
+		var pos = gameObject.transform.localPosition;
+		pos.x = xOff;
+		pos.y = yOff;
+		gameObject.transform.localPosition = pos;
+	}
+}
