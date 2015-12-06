68ec134acbc25a548c19155dfb5bc1aec629aa0e
Thamstras <thomas_casson@btinternet.com>
2015-12-06 02:31:14 +0000

    I think we're done!

---
+++ JincJam/Assets/DistanceScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DistanceScript : MonoBehaviour {
+
+	private GameObject player;
+	private float maxDist = 0.0f;
+	private Text text;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		text = gameObject.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.gameObject == null)
+			return;
+		if (player.transform.position.x > maxDist)
+		{
+			maxDist = player.transform.position.x;
+		}
+		text.text = string.Concat ("Distance Travelled:\n", Mathf.FloorToInt(maxDist/10.0f));
+	}
+}
